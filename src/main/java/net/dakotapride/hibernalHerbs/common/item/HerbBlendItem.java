a9b5aba82b1c3b5526c3c511de6756739ca1409a
DakotaPride <rjtube13@gmail.com>
2022-11-19 22:44:02 -0600

    v0.2.0 - 'Pvp' Mechanics

--- src/main/java/net/dakotapride/hibernalHerbs/common/item/HerbBlendItem.java
+++ src/main/java/net/dakotapride/hibernalHerbs/common/item/HerbBlendItem.java
@@ -3,12 +3,16 @@ package net.dakotapride.hibernalHerbs.common.item;
 import net.dakotapride.hibernalHerbs.common.init.ItemInit;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
@@ -17,6 +21,72 @@ import java.util.List;
 public class HerbBlendItem extends Item {
     public HerbBlendItem(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
+        if (user.getStackInHand(Hand.MAIN_HAND).isOf(ItemInit.REGENERATIVE_BLEND)) {
+            entity.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 300, 1));
+        } else if (user.getStackInHand(Hand.MAIN_HAND).isOf(ItemInit.VIRULENT_BLEND)) {
+            entity.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 300, 1));
+        } else if (user.getStackInHand(Hand.MAIN_HAND).isOf(ItemInit.SEDATING_BLEND)) {
+            entity.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 300, 1));
+        } else if (user.getStackInHand(Hand.MAIN_HAND).isOf(ItemInit.HINDERING_BLEND)) {
+            entity.addStatusEffect(new StatusEffectInstance(StatusEffects.MINING_FATIGUE, 300, 1));
+        } else if (user.getStackInHand(Hand.MAIN_HAND).isOf(ItemInit.DASHING_BLEND)) {
+            entity.addStatusEffect(new StatusEffectInstance(StatusEffects.HASTE, 300, 1));
+        } else if (user.getStackInHand(Hand.MAIN_HAND).isOf(ItemInit.ACCELERATION_BLEND)) {
+            entity.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 300, 1));
+        } else if (user.getStackInHand(Hand.MAIN_HAND).isOf(ItemInit.INCINERATING_BLEND)) {
+            entity.setFireTicks(300);
+        } else if (user.getStackInHand(Hand.MAIN_HAND).isOf(ItemInit.DECAYING_BLEND)) {
+            entity.addStatusEffect(new StatusEffectInstance(StatusEffects.WITHER, 300, 1));
+        } else if (user.getStackInHand(Hand.MAIN_HAND).isOf(ItemInit.OBSERVING_BLEND)) {
+            entity.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 300, 1));
+        } else if (user.getStackInHand(Hand.MAIN_HAND).isOf(ItemInit.DIMINISHED_BLEND)) {
+            entity.addStatusEffect(new StatusEffectInstance(StatusEffects.WEAKNESS, 300, 1));
+        } else if (user.getStackInHand(Hand.MAIN_HAND).isOf(ItemInit.SHADED_BLEND)) {
+            entity.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 300, 1));
+        }
+
+        stack.decrement(1);
+        user.giveItemStack(new ItemStack(Items.BOWL, 1));
+
+        return super.useOnEntity(stack, user, entity, hand);
+    }
+
+    @Override
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        if (attacker instanceof PlayerEntity playerEntity) {
+            if (attacker.getStackInHand(Hand.MAIN_HAND).isOf(ItemInit.REGENERATIVE_BLEND)) {
+                target.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 140, 0));
+            } else if (attacker.getStackInHand(Hand.MAIN_HAND).isOf(ItemInit.VIRULENT_BLEND)) {
+                target.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 140, 0));
+            } else if (attacker.getStackInHand(Hand.MAIN_HAND).isOf(ItemInit.SEDATING_BLEND)) {
+                target.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 140, 0));
+            } else if (attacker.getStackInHand(Hand.MAIN_HAND).isOf(ItemInit.HINDERING_BLEND)) {
+                target.addStatusEffect(new StatusEffectInstance(StatusEffects.MINING_FATIGUE, 140, 0));
+            } else if (attacker.getStackInHand(Hand.MAIN_HAND).isOf(ItemInit.DASHING_BLEND)) {
+                target.addStatusEffect(new StatusEffectInstance(StatusEffects.HASTE, 140, 0));
+            } else if (attacker.getStackInHand(Hand.MAIN_HAND).isOf(ItemInit.ACCELERATION_BLEND)) {
+                target.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 140, 0));
+            } else if (attacker.getStackInHand(Hand.MAIN_HAND).isOf(ItemInit.INCINERATING_BLEND)) {
+                target.setFireTicks(140);
+            } else if (attacker.getStackInHand(Hand.MAIN_HAND).isOf(ItemInit.DECAYING_BLEND)) {
+                target.addStatusEffect(new StatusEffectInstance(StatusEffects.WITHER, 140, 0));
+            } else if (attacker.getStackInHand(Hand.MAIN_HAND).isOf(ItemInit.OBSERVING_BLEND)) {
+                target.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 140, 0));
+            } else if (attacker.getStackInHand(Hand.MAIN_HAND).isOf(ItemInit.DIMINISHED_BLEND)) {
+                target.addStatusEffect(new StatusEffectInstance(StatusEffects.WEAKNESS, 140, 0));
+            } else if (attacker.getStackInHand(Hand.MAIN_HAND).isOf(ItemInit.SHADED_BLEND)) {
+                target.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 140, 0));
+            }
+
+            stack.decrement(1);
+            playerEntity.giveItemStack(new ItemStack(Items.BOWL, 1));
+        }
+
+        return super.postHit(stack, target, attacker);
     }
 
     @Override
