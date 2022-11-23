afbcb21edf72736e9ad07563adb01a83f522ea25
DakotaPride <rjtube13@gmail.com>
2022-11-22 21:44:28 -0600

    v0.2.0 - Polishing herb/tree worldgen

--- src/main/java/net/dakotapride/hibernalHerbs/common/init/FeaturesInit.java
+++ src/main/java/net/dakotapride/hibernalHerbs/common/init/FeaturesInit.java
@@ -25,55 +25,55 @@ public class FeaturesInit {
 
     public static final RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> ROSEMARY_CONFIGURED =
             ConfiguredFeatures.register(HIBERNAL_HERBS_ID + ":herb_rosemary_configured", Feature.FLOWER,
-                    ConfiguredFeatures.createRandomPatchFeatureConfig(64, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
+                    ConfiguredFeatures.createRandomPatchFeatureConfig(16, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
                             new SimpleBlockFeatureConfig(BlockStateProvider.of(BlockInit.ROSEMARY)))));
     public static final RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> THYME_CONFIGURED =
             ConfiguredFeatures.register(HIBERNAL_HERBS_ID + ":herb_thyme_configured", Feature.FLOWER,
-                    ConfiguredFeatures.createRandomPatchFeatureConfig(64, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
+                    ConfiguredFeatures.createRandomPatchFeatureConfig(16, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
                             new SimpleBlockFeatureConfig(BlockStateProvider.of(BlockInit.THYME)))));
     public static final RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> TARRAGON_CONFIGURED =
             ConfiguredFeatures.register(HIBERNAL_HERBS_ID + ":herb_tarragon_configured", Feature.FLOWER,
-                    ConfiguredFeatures.createRandomPatchFeatureConfig(64, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
+                    ConfiguredFeatures.createRandomPatchFeatureConfig(16, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
                             new SimpleBlockFeatureConfig(BlockStateProvider.of(BlockInit.TARRAGON)))));
     public static final RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> CHAMOMILE_CONFIGURED =
             ConfiguredFeatures.register(HIBERNAL_HERBS_ID + ":herb_chamomile_configured", Feature.FLOWER,
-                    ConfiguredFeatures.createRandomPatchFeatureConfig(64, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
+                    ConfiguredFeatures.createRandomPatchFeatureConfig(16, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
                             new SimpleBlockFeatureConfig(BlockStateProvider.of(BlockInit.CHAMOMILE)))));
     public static final RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> CHIVES_CONFIGURED =
             ConfiguredFeatures.register(HIBERNAL_HERBS_ID + ":herb_chives_configured", Feature.FLOWER,
-                    ConfiguredFeatures.createRandomPatchFeatureConfig(64, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
+                    ConfiguredFeatures.createRandomPatchFeatureConfig(16, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
                             new SimpleBlockFeatureConfig(BlockStateProvider.of(BlockInit.CHIVES)))));
     public static final RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> VERBENA_CONFIGURED =
             ConfiguredFeatures.register(HIBERNAL_HERBS_ID + ":herb_verbena_configured", Feature.FLOWER,
-                    ConfiguredFeatures.createRandomPatchFeatureConfig(64, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
+                    ConfiguredFeatures.createRandomPatchFeatureConfig(16, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
                             new SimpleBlockFeatureConfig(BlockStateProvider.of(BlockInit.VERBENA)))));
     public static final RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> SORREL_CONFIGURED =
             ConfiguredFeatures.register(HIBERNAL_HERBS_ID + ":herb_sorrel_configured", Feature.FLOWER,
-                    ConfiguredFeatures.createRandomPatchFeatureConfig(64, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
+                    ConfiguredFeatures.createRandomPatchFeatureConfig(16, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
                             new SimpleBlockFeatureConfig(BlockStateProvider.of(BlockInit.SORREL)))));
     public static final RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> MARJORAM_CONFIGURED =
             ConfiguredFeatures.register(HIBERNAL_HERBS_ID + ":herb_marjoram_configured", Feature.FLOWER,
-                    ConfiguredFeatures.createRandomPatchFeatureConfig(64, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
+                    ConfiguredFeatures.createRandomPatchFeatureConfig(16, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
                             new SimpleBlockFeatureConfig(BlockStateProvider.of(BlockInit.MARJORAM)))));
     public static final RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> CHERVIL_CONFIGURED =
             ConfiguredFeatures.register(HIBERNAL_HERBS_ID + ":herb_chervil_configured", Feature.FLOWER,
-                    ConfiguredFeatures.createRandomPatchFeatureConfig(64, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
+                    ConfiguredFeatures.createRandomPatchFeatureConfig(16, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
                             new SimpleBlockFeatureConfig(BlockStateProvider.of(BlockInit.CHERVIL)))));
     public static final RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> FENNSEL_CONFIGURED =
             ConfiguredFeatures.register(HIBERNAL_HERBS_ID + ":herb_fennsel_configured", Feature.FLOWER,
-                    ConfiguredFeatures.createRandomPatchFeatureConfig(64, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
+                    ConfiguredFeatures.createRandomPatchFeatureConfig(16, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
                             new SimpleBlockFeatureConfig(BlockStateProvider.of(BlockInit.FENNSEL)))));
     public static final RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> CEILLIS_CONFIGURED =
             ConfiguredFeatures.register(HIBERNAL_HERBS_ID + ":herb_ceillis_configured", Feature.FLOWER,
-                    ConfiguredFeatures.createRandomPatchFeatureConfig(64, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
+                    ConfiguredFeatures.createRandomPatchFeatureConfig(16, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
                             new SimpleBlockFeatureConfig(BlockStateProvider.of(BlockInit.CEILLIS)))));
     public static final RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> PUNUEL_CONFIGURED =
             ConfiguredFeatures.register(HIBERNAL_HERBS_ID + ":herb_punuel_configured", Feature.FLOWER,
-                    ConfiguredFeatures.createRandomPatchFeatureConfig(64, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
+                    ConfiguredFeatures.createRandomPatchFeatureConfig(16, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
                             new SimpleBlockFeatureConfig(BlockStateProvider.of(BlockInit.PUNUEL)))));
     public static final RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> ESSITTE_CONFIGURED =
             ConfiguredFeatures.register(HIBERNAL_HERBS_ID + ":herb_essitte_configured", Feature.FLOWER,
-                    ConfiguredFeatures.createRandomPatchFeatureConfig(64, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
+                    ConfiguredFeatures.createRandomPatchFeatureConfig(16, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
                             new SimpleBlockFeatureConfig(BlockStateProvider.of(BlockInit.ESSITTE)))));
 
     public static final RegistryEntry<ConfiguredFeature<TreeFeatureConfig, ?>> MYQUESTE_CONFIGURED =
@@ -90,54 +90,54 @@ public class FeaturesInit {
 
     public static final RegistryEntry<ConfiguredFeature<RandomFeatureConfig, ?>> MYQUESTE_SPAWN =
             ConfiguredFeatures.register(HIBERNAL_HERBS_ID + ":myqueste_spawn", Feature.RANDOM_SELECTOR,
-                    new RandomFeatureConfig(List.of(new RandomFeatureEntry(MYQUESTE_CHECKED, 0.5f)),
+                    new RandomFeatureConfig(List.of(new RandomFeatureEntry(MYQUESTE_CHECKED, 0.2f)),
                             MYQUESTE_CHECKED));
 
     // Placed
 
     public static final RegistryEntry<PlacedFeature> ROSEMARY_PLACED = PlacedFeatures.register(HIBERNAL_HERBS_ID + ":rosemary_placed",
-            ROSEMARY_CONFIGURED, RarityFilterPlacementModifier.of(4), SquarePlacementModifier.of(),
+            ROSEMARY_CONFIGURED, RarityFilterPlacementModifier.of(2), SquarePlacementModifier.of(),
             PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP, BiomePlacementModifier.of());
     public static final RegistryEntry<PlacedFeature> THYME_PLACED = PlacedFeatures.register(HIBERNAL_HERBS_ID + ":thyme_placed",
-            THYME_CONFIGURED, RarityFilterPlacementModifier.of(4), SquarePlacementModifier.of(),
+            THYME_CONFIGURED, RarityFilterPlacementModifier.of(2), SquarePlacementModifier.of(),
             PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP, BiomePlacementModifier.of());
     public static final RegistryEntry<PlacedFeature> TARRAGON_PLACED = PlacedFeatures.register(HIBERNAL_HERBS_ID + ":tarragon_placed",
-            TARRAGON_CONFIGURED, RarityFilterPlacementModifier.of(4), SquarePlacementModifier.of(),
+            TARRAGON_CONFIGURED, RarityFilterPlacementModifier.of(2), SquarePlacementModifier.of(),
             PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP, BiomePlacementModifier.of());
     public static final RegistryEntry<PlacedFeature> CHAMOMILE_PLACED = PlacedFeatures.register(HIBERNAL_HERBS_ID + ":chamomile_placed",
-            CHAMOMILE_CONFIGURED, RarityFilterPlacementModifier.of(4), SquarePlacementModifier.of(),
+            CHAMOMILE_CONFIGURED, RarityFilterPlacementModifier.of(2), SquarePlacementModifier.of(),
             PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP, BiomePlacementModifier.of());
     public static final RegistryEntry<PlacedFeature> CHIVES_PLACED = PlacedFeatures.register(HIBERNAL_HERBS_ID + ":chives_placed",
-            CHIVES_CONFIGURED, RarityFilterPlacementModifier.of(4), SquarePlacementModifier.of(),
+            CHIVES_CONFIGURED, RarityFilterPlacementModifier.of(2), SquarePlacementModifier.of(),
             PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP, BiomePlacementModifier.of());
     public static final RegistryEntry<PlacedFeature> VERBENA_PLACED = PlacedFeatures.register(HIBERNAL_HERBS_ID + ":verbena_placed",
-            VERBENA_CONFIGURED, RarityFilterPlacementModifier.of(4), SquarePlacementModifier.of(),
+            VERBENA_CONFIGURED, RarityFilterPlacementModifier.of(2), SquarePlacementModifier.of(),
             PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP, BiomePlacementModifier.of());
     public static final RegistryEntry<PlacedFeature> SORREL_PLACED = PlacedFeatures.register(HIBERNAL_HERBS_ID + ":sorrel_placed",
-            SORREL_CONFIGURED, RarityFilterPlacementModifier.of(4), SquarePlacementModifier.of(),
+            SORREL_CONFIGURED, RarityFilterPlacementModifier.of(2), SquarePlacementModifier.of(),
             PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP, BiomePlacementModifier.of());
     public static final RegistryEntry<PlacedFeature> MARJORAM_PLACED = PlacedFeatures.register(HIBERNAL_HERBS_ID + ":marjoram_placed",
-            MARJORAM_CONFIGURED, RarityFilterPlacementModifier.of(4), SquarePlacementModifier.of(),
+            MARJORAM_CONFIGURED, RarityFilterPlacementModifier.of(2), SquarePlacementModifier.of(),
             PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP, BiomePlacementModifier.of());
     public static final RegistryEntry<PlacedFeature> CHERVIL_PLACED = PlacedFeatures.register(HIBERNAL_HERBS_ID + ":chervil_placed",
-            CHERVIL_CONFIGURED, RarityFilterPlacementModifier.of(4), SquarePlacementModifier.of(),
+            CHERVIL_CONFIGURED, RarityFilterPlacementModifier.of(2), SquarePlacementModifier.of(),
             PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP, BiomePlacementModifier.of());
     public static final RegistryEntry<PlacedFeature> FENNSEL_PLACED = PlacedFeatures.register(HIBERNAL_HERBS_ID + ":fennsel_placed",
-            FENNSEL_CONFIGURED, RarityFilterPlacementModifier.of(4), SquarePlacementModifier.of(),
+            FENNSEL_CONFIGURED, RarityFilterPlacementModifier.of(2), SquarePlacementModifier.of(),
             PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP, BiomePlacementModifier.of());
     public static final RegistryEntry<PlacedFeature> CEILLIS_PLACED = PlacedFeatures.register(HIBERNAL_HERBS_ID + ":ceillis_placed",
-            CEILLIS_CONFIGURED, RarityFilterPlacementModifier.of(4), SquarePlacementModifier.of(),
+            CEILLIS_CONFIGURED, RarityFilterPlacementModifier.of(2), SquarePlacementModifier.of(),
             PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP, BiomePlacementModifier.of());
     public static final RegistryEntry<PlacedFeature> PUNUEL_PLACED = PlacedFeatures.register(HIBERNAL_HERBS_ID + ":punuel_placed",
-            PUNUEL_CONFIGURED, RarityFilterPlacementModifier.of(4), SquarePlacementModifier.of(),
+            PUNUEL_CONFIGURED, RarityFilterPlacementModifier.of(2), SquarePlacementModifier.of(),
             PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP, BiomePlacementModifier.of());
     public static final RegistryEntry<PlacedFeature> ESSITTE_PLACED = PlacedFeatures.register(HIBERNAL_HERBS_ID + ":essitte_placed",
-            ESSITTE_CONFIGURED, RarityFilterPlacementModifier.of(4), SquarePlacementModifier.of(),
+            ESSITTE_CONFIGURED, RarityFilterPlacementModifier.of(2), SquarePlacementModifier.of(),
             PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP, BiomePlacementModifier.of());
 
     public static final RegistryEntry<PlacedFeature> MYQUESTE_PLACED =
             PlacedFeatures.register(HIBERNAL_HERBS_ID + ":myqueste_placed", MYQUESTE_SPAWN,
-                    VegetationPlacedFeatures.modifiers(PlacedFeatures.createCountExtraModifier(1, 0.1f, 2)));
+                    VegetationPlacedFeatures.modifiers(PlacedFeatures.createCountExtraModifier(1, 0.1f, 1)));
 
     public static void init() {
         // Load This Class
