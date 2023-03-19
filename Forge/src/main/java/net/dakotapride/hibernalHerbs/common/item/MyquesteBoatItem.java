617384506f0df620561368dd8eb7b1a1c3cc24db
DakotaPride <rjtube13@gmail.com>
2023-03-18 22:18:09 -0500

    v0.7.2 - Fix Textures for Boats [Forge]

--- Forge/src/main/java/net/dakotapride/hibernalHerbs/common/item/MyquesteBoatItem.java
+++ Forge/src/main/java/net/dakotapride/hibernalHerbs/common/item/MyquesteBoatItem.java
@@ -2,6 +2,8 @@ package net.dakotapride.hibernalHerbs.common.item;
 
 import net.dakotapride.hibernalHerbs.common.entity.boat.MyquesteBoat;
 import net.dakotapride.hibernalHerbs.common.entity.boat.MyquesteChestBoat;
+import net.minecraft.client.gui.components.ChatComponent;
+import net.minecraft.network.chat.Component;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
@@ -10,12 +12,14 @@ import net.minecraft.world.entity.EntitySelector;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.function.Predicate;
@@ -25,6 +29,7 @@ public class MyquesteBoatItem extends Item {
 
     private final boolean hasChest;
     private final MyquesteBoat.MyquesteType type;
+    boolean isSafe = false;
 
     public MyquesteBoatItem(boolean hasChest, MyquesteBoat.MyquesteType typeIn, Properties properties) {
         super(properties);
@@ -33,8 +38,15 @@ public class MyquesteBoatItem extends Item {
     }
 
     @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> componentList, TooltipFlag tooltipFlag) {
+        if (!isSafe) {
+            componentList.add(Component.literal("Item is Currently Disabled Due To Known Issues"));
+            componentList.add(Component.literal("Apologies For The Inconvenience!"));
+        }
+    }
+
+    @Override
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
-        boolean isSafe = false;
         if (isSafe) {
             ItemStack itemstack = pPlayer.getItemInHand(pHand);
             HitResult hitresult = getPlayerPOVHitResult(pLevel, pPlayer, ClipContext.Fluid.ANY);
