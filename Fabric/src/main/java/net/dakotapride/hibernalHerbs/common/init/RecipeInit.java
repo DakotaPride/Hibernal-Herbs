e62055dd8d92560b0673fb8139db74c0f3a16792
DakotaPride <rjtube13@gmail.com>
2023-03-28 14:48:39 -0500

    v0.8 - Misc change

--- Fabric/src/main/java/net/dakotapride/hibernalHerbs/common/init/RecipeInit.java
+++ Fabric/src/main/java/net/dakotapride/hibernalHerbs/common/init/RecipeInit.java
@@ -1,16 +1,22 @@
 package net.dakotapride.hibernalHerbs.common.init;
 
 import net.dakotapride.hibernalHerbs.common.recipe.HerbalConjurationRecipe;
+import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.recipe.RecipeType;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 import static net.dakotapride.hibernalHerbs.common.Constants.MOD_ID;
 
 public class RecipeInit {
+
+    public static RecipeSerializer<HerbalConjurationRecipe> HERBAL_CONJURATION_SERIALIZER;
+    public static RecipeType<HerbalConjurationRecipe> HERBAL_CONJURATION_TYPE;
+
     public static void init() {
-        Registry.register(Registry.RECIPE_SERIALIZER, new Identifier(MOD_ID, HerbalConjurationRecipe.Serializer.ID),
-                HerbalConjurationRecipe.Serializer.INSTANCE);
-        Registry.register(Registry.RECIPE_TYPE, new Identifier(MOD_ID, HerbalConjurationRecipe.Type.ID),
-                HerbalConjurationRecipe.Type.INSTANCE);
+        HERBAL_CONJURATION_SERIALIZER = Registry.register(Registry.RECIPE_SERIALIZER,
+                new Identifier(MOD_ID, HerbalConjurationRecipe.Serializer.ID), HerbalConjurationRecipe.Serializer.INSTANCE);
+        HERBAL_CONJURATION_TYPE = Registry.register(Registry.RECIPE_TYPE,
+                new Identifier(MOD_ID, HerbalConjurationRecipe.Type.ID), HerbalConjurationRecipe.Type.INSTANCE);
     }
 }
--- Forge/src/main/java/net/dakotapride/hibernalHerbs/common/recipe/HerbalConjurationRecipe.java
+++ Forge/src/main/java/net/dakotapride/hibernalHerbs/common/recipe/HerbalConjurationRecipe.java
@@ -1,7 +1,10 @@
 package net.dakotapride.hibernalHerbs.common.recipe;
 
+import com.google.common.collect.Maps;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
 import net.minecraft.core.NonNullList;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
@@ -15,6 +18,7 @@ import javax.annotation.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static net.dakotapride.hibernalHerbs.common.Constants.MOD_ID;
 
@@ -100,6 +104,48 @@ public class HerbalConjurationRecipe implements Recipe<SimpleContainer> {
         public static final String ID = "herbal_conjuration";
     }
 
+    static String[] patternFromJson(JsonArray p_44197_) {
+        String[] astring = new String[p_44197_.size()];
+        if (astring.length > 2) {
+            throw new JsonSyntaxException("Invalid pattern: too many rows, " + 2 + " is maximum");
+        } else if (astring.length == 0) {
+            throw new JsonSyntaxException("Invalid pattern: empty pattern not allowed");
+        } else {
+            for(int i = 0; i < astring.length; ++i) {
+                String s = GsonHelper.convertToString(p_44197_.get(i), "pattern[" + i + "]");
+                if (s.length() > 3) {
+                    throw new JsonSyntaxException("Invalid pattern: too many columns, " + 3 + " is maximum");
+                }
+
+                if (i > 0 && astring[0].length() != s.length()) {
+                    throw new JsonSyntaxException("Invalid pattern: each row must be the same width");
+                }
+
+                astring[i] = s;
+            }
+
+            return astring;
+        }
+    }
+
+    static Map<String, Ingredient> keyFromJson(JsonObject p_44211_) {
+        Map<String, Ingredient> map = Maps.newHashMap();
+
+        for(Map.Entry<String, JsonElement> entry : p_44211_.entrySet()) {
+            if (entry.getKey().length() != 1) {
+                throw new JsonSyntaxException("Invalid key entry: '" + (String)entry.getKey() + "' is an invalid symbol (must be 1 character only).");
+            }
+
+            if (" ".equals(entry.getKey())) {
+                throw new JsonSyntaxException("Invalid key entry: ' ' is a reserved symbol.");
+            }
+
+            map.put(entry.getKey(), Ingredient.fromJson(entry.getValue()));
+        }
+
+        map.put(" ", Ingredient.EMPTY);
+        return map;
+    }
 
     public static class Serializer implements RecipeSerializer<HerbalConjurationRecipe> {
         public static final Serializer INSTANCE = new Serializer();
@@ -109,8 +155,6 @@ public class HerbalConjurationRecipe implements Recipe<SimpleContainer> {
         @Override
         public HerbalConjurationRecipe fromJson(ResourceLocation recipeId, JsonObject serializedRecipe) {
             ItemStack output = ShapedRecipe.itemStackFromJson(GsonHelper.getAsJsonObject(serializedRecipe, "output"));
-
-            JsonArray pattern = GsonHelper.getAsJsonArray(serializedRecipe, "pattern");
 
             JsonArray ingredients = GsonHelper.getAsJsonArray(serializedRecipe, "ingredients");
             NonNullList<Ingredient> inputs = NonNullList.withSize(6, Ingredient.EMPTY);
