da0b483d5917a99ee171a1d98f3465a7bf15139c
Dakota <rjtube13@gmail.com>
2025-03-13 12:23:46 -0500

    1.4.1.1 - Item Descriptions Compatibility

--- src/main/java/net/dakotapride/hibernalherbs/item/LifeForceBottleItem.java
+++ src/main/java/net/dakotapride/hibernalherbs/item/LifeForceBottleItem.java
@@ -11,6 +11,7 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -38,7 +39,7 @@ public class LifeForceBottleItem extends Item {
         Level level = useOnContext.getLevel();
         BlockPos blockPos = useOnContext.getClickedPos();
         BlockState blockState = level.getBlockState(blockPos);
-        return (InteractionResult) getDeterioratedState(blockState).map(blockStatex -> {
+        return getDeterioratedState(blockState).map(blockStatex -> {
             Player player = useOnContext.getPlayer();
             ItemStack itemStack = useOnContext.getItemInHand();
             if (player instanceof ServerPlayer) {
@@ -46,6 +47,7 @@ public class LifeForceBottleItem extends Item {
             }
 
             itemStack.shrink(1);
+            player.addItem(new ItemStack(Items.GLASS_BOTTLE, 1));
             level.setBlock(blockPos, blockStatex, 11);
             level.gameEvent(GameEvent.BLOCK_CHANGE, blockPos, GameEvent.Context.of(player, blockStatex));
             //level.levelEvent(player, 3003, blockPos, 0);
